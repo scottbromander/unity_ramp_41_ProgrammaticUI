743853759284e9c63a8146087c5dcf8e66c24228
Scott Bromander <scott@primeacademy.io>
2016-10-11 15:59:31 -0500

    scale test

--- Assets/Decay.cs
+++ Assets/Decay.cs
@@ -17,6 +17,10 @@ public class Decay : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		uiImage.color = new Color (uiImage.color.r, uiImage.color.g, uiImage.color.b, ((decaySeconds - elapsedSeconds) / decaySeconds));
+
+		float scale = 1 + (elapsedSeconds * 2);
+		uiImage.gameObject.transform.localScale = new Vector3 (scale, scale, scale);
+
 		elapsedSeconds += Time.deltaTime;
 	}
 }
