849b6df82101555b0387cf841018af6d46d82052
Scott Bromander <scott@primeacademy.io>
2016-10-11 16:39:30 -0500

    random direction script

---
+++ Assets/Scripts/MoveInDirection.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveInDirection : MonoBehaviour {
+
+	Vector2 direction;
+	float maxRange = 5;
+
+	// Use this for initialization
+	void Start () {
+		direction = new Vector2 (Random.Range (-maxRange, maxRange), Random.Range (-maxRange, maxRange));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (direction.x, direction.y, 0);
+	}
+}
