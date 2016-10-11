adbbe64637a63927024a16a0ba6b93008b0c28f2
Scott Bromander <scott@primeacademy.io>
2016-10-11 15:57:51 -0500

    image fade and destroy

---
+++ Assets/Decay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Decay : MonoBehaviour {
+
+	public float decaySeconds = 1f;
+	float elapsedSeconds = 0;
+	Image uiImage;
+
+	// Use this for initialization
+	void Start () {
+		uiImage = GetComponent<Image> ();
+		Destroy (this.gameObject, decaySeconds);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		uiImage.color = new Color (uiImage.color.r, uiImage.color.g, uiImage.color.b, ((decaySeconds - elapsedSeconds) / decaySeconds));
+		elapsedSeconds += Time.deltaTime;
+	}
+}
