1b3c78a224973031f741a8ed5ff044cbae5c3830
Scott Bromander <scott@primeacademy.io>
2016-10-11 16:11:57 -0500

    click to create word object

---
+++ Assets/Scripts/ClickWord.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickWord : MonoBehaviour {
+
+	public GameObject wordPrefab;
+
+	// Use this for initialization
+	void Start () {
+		if (wordPrefab == null) {
+			Debug.Log ("You must the Word Prefab");
+			this.enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Vector3 clickPosition = Input.mousePosition;
+			GameObject wordObject = Instantiate (wordPrefab, clickPosition, Quaternion.identity) as GameObject;
+			wordObject.transform.SetParent (this.transform);
+		}
+	}
+}
