0e2c3a06262833695265b6599a6cc1bf89793781
Scott Bromander <scott@primeacademy.io>
2016-10-12 17:31:42 -0500

    image constructor

---
+++ Assets/Scripts/HitEffectConstructor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HitEffectConstructor : MonoBehaviour {
+
+	public Sprite spriteToUse;
+	public bool hasRandomMovement;
+	public float decayTime;
+
+	// Use this for initialization
+	void Start () {
+		Decay decayScript = GetComponent<Decay> ();
+		if (decayScript != null) {
+			decayScript.decaySeconds = decayTime;
+		}
+
+		if (hasRandomMovement) {
+			this.gameObject.AddComponent<MoveInDirection> ();
+		}
+
+		Image imageScript = this.GetComponent<Image> ();
+		imageScript.sprite = spriteToUse;
+	}
+}
