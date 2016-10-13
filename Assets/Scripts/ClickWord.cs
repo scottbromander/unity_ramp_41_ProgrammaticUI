efb443b98c0ed3d6eebf385be0e4799036696b65
Scott Bromander <scott@primeacademy.io>
2016-10-13 12:59:47 -0500

    deelegate example

--- Assets/Scripts/ClickWord.cs
+++ Assets/Scripts/ClickWord.cs
@@ -1,14 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ClickWord : MonoBehaviour {
 
-	public GameObject wordPrefab;
+	public List<GameObject> hitEffects = new List<GameObject> ();
 
 	// Use this for initialization
 	void Start () {
-		if (wordPrefab == null) {
-			Debug.Log ("You must the Word Prefab");
+		if (hitEffects.Count < 1) {
+			Debug.Log ("You must add at least one effect!");
 			this.enabled = false;
 		}
 	}
@@ -17,7 +18,7 @@ public class ClickWord : MonoBehaviour {
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
 			Vector3 clickPosition = Input.mousePosition;
-			GameObject wordObject = Instantiate (wordPrefab, clickPosition, Quaternion.identity) as GameObject;
+			GameObject wordObject = Instantiate (hitEffects[Random.Range(0, hitEffects.Count)], clickPosition, Quaternion.identity) as GameObject;
 			wordObject.transform.SetParent (this.transform);
 		}
 	}
---
+++ Assets/Scripts/DelegateExample.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelegateExample : MonoBehaviour {
+
+	delegate void ExampleDelegate();
+
+	void ThisIsADelegate(){
+		Debug.Log ("Here we are!");
+	}
+
+	// Use this for initialization
+	void Start () {
+		ExampleDelegate methodToUse = ThisIsADelegate;
+
+		methodToUse.Invoke ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
