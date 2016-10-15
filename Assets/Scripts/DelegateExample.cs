7c4ca5f490aadd0893979fa8dbd55e28c92b7b34
Scott Bromander <scott@primeacademy.io>
2016-10-14 20:12:34 -0500

    delegate swap on click test

--- Assets/Scripts/DelegateExample.cs
+++ Assets/Scripts/DelegateExample.cs
@@ -4,17 +4,25 @@ using System.Collections;
 public class DelegateExample : MonoBehaviour {
 
 	delegate void ExampleDelegate(int a, int b);
+	ExampleDelegate methodToUse;
 
 	void AddDelegate(int a, int b){
 		Debug.Log ("Here is the sum: " + (a + b));
+		methodToUse = SubtractDelegate;
 	}
 
 	void SubtractDelegate(int a, int b){
 		Debug.Log ("Here is the difference: " + (a - b));
+		methodToUse = AddDelegate;
 	}
 
 	void Start(){
-		ExampleDelegate methodToUse = SubtractDelegate;
-		methodToUse.Invoke (4, 5);
+		methodToUse = SubtractDelegate;
+	}
+
+	void Update(){
+		if (Input.GetMouseButtonDown (0)) {
+			methodToUse.Invoke (4, 5);
+		}
 	}
 }
