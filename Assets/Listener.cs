9e931c1c2c82c7dfba89ee58764e1b275abc57ab
Scott Bromander <scott@primeacademy.io>
2016-10-16 21:10:25 -0500

    ecustom events and listeners

---
+++ Assets/Listener.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Listener : MonoBehaviour {
+
+	public GameObject gameObjectWithEvents;
+
+	// Use this for initialization
+	void Start () {
+		if (gameObjectWithEvents == null) {
+			this.enabled = false;
+			Debug.Log ("Must Set Game Object With Events");
+		} else { 
+			gameObjectWithEvents.GetComponent<Events> ().MouseLeftClick += HandleEvent;
+		}
+	}
+
+	public void HandleEvent(GameObject sender, string eventMessage){
+		Debug.Log (sender.name + " sent us an event: " + eventMessage);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
