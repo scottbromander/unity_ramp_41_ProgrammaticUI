5b197ba41896a912a4fb9938890f32aeef4b5134
Scott Bromander <scott@primeacademy.io>
2016-10-17 19:48:23 -0500

    cuustom mouse events

---
+++ Assets/Scripts/OurButtonHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OurButtonHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Button button = this.GetComponent<Button> ();
+		button.onClick.RemoveAllListeners ();
+		button.onClick.AddListener (()=> HandleButton(button));
+	}
+
+	void HandleButton(Button button){
+		button.GetComponent<Image> ().color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
