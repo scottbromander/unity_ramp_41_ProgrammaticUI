5b6508e7910857adb3766b9bfa395a16c29bf78a
Scott Bromander <scott@primeacademy.io>
2016-10-20 20:19:22 -0500

    implemented interfaces for event handler class

---
+++ Assets/Scripts/MyEventHandlers.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class MyEventHandlers : MonoBehaviour, IPointerEnterHandler, IDragHandler, IPointerExitHandler {
+
+	private bool pulse = false;
+	private float time = 0;
+
+	public void OnDrag(PointerEventData eventData) {
+		
+	}
+
+	public void OnPointerEnter(PointerEventData eventData) {
+
+	}
+
+	public void OnPointerExit(PointerEventData eventData) {
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
