a6d163685bfa850ea65b1920120c5b4f292867d3
Scott Bromander <scott@primeacademy.io>
2017-05-31 21:18:54 -0500

    programmic UI course update

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	Canvas baseCanvas;
+
+	// Use this for initialization
+	void Start () {
+		GameObject canvas = new GameObject ("Canvas");
+		baseCanvas = canvas.AddComponent<Canvas> ();
+		baseCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+
+		canvas.AddComponent<CanvasScaler> ();
+
+		canvas.AddComponent<GraphicRaycaster> (); //allows UI to understand mouse events
+
+		GameObject eventSystem = new GameObject ("EventSystem");
+		eventSystem.AddComponent<UnityEngine.EventSystems.EventSystem> ();
+		eventSystem.AddComponent<UnityEngine.EventSystems.StandaloneInputModule> ();
+
+	}
+
+	void CreateSprite() {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
