d36839b8634a3ac8b1aabe5e8917ec1ac62d8a49
Scott Bromander <scott@primeacademy.io>
2017-06-21 21:06:48 -0500

    distance alpha

---
+++ Assets/Scripts/VisableRange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisableRange : MonoBehaviour {
+
+	CanvasGroup canvasGroup;
+	public float visableDistance = 5f;
+
+	// Use this for initialization
+	void Start () {
+		canvasGroup = this.GetComponent<CanvasGroup> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distance = (this.transform.position - Camera.main.transform.position).magnitude;
+		float alpha = visableDistance - distance;
+
+		canvasGroup.alpha = Mathf.Clamp (alpha, 0, 1);
+	}
+}
